0b20242edb9d34c62d2b991e49f8e245a693618b
HillsonShalom <hillshaloz@gmail.com>
2024-10-13 12:03:01 +0300

    classroom route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import exp from 'express'
 import authRtr from './routes/authRouter'
-import teachersRtr from './routes/teacherRouter'
-import studentsRtr from './routes/studentsRouter'
+import teachersRtr  from './routes/teacherRouter'
+import studentsRtr  from './routes/studentsRouter'
+import classroomRtr from './routes/classroomRouter'
 
 import 'dotenv/config'
 const port = process.env.PORT
@@ -10,9 +11,10 @@ const app = exp()
 
 app.use(exp.json())
 
-app.use('/auth'    , authRtr    );
-app.use('/students', studentsRtr);
-app.use('/teachers', teachersRtr);
+app.use('/auth'     , authRtr     );
+app.use('/students' , studentsRtr );
+app.use('/teachers' , teachersRtr );
+app.use('/classroom', classroomRtr);
 
 app.listen(port, () => {
     console.log(`Listening on 0.0.0.0:${port}...`);
