7aa2fa7c473ad95febcb71783d9b426a83bd952e
HillsonShalom <hillshaloz@gmail.com>
2024-10-13 11:34:53 +0300

    connecting routers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import exp from 'express'
+import authRtr from './routes/authRouter'
+import teachersRtr from './routes/teacherRouter'
+import studentsRtr from './routes/studentsRouter'
+
 import 'dotenv/config'
 const port = process.env.PORT
 
 const app = exp()
 
 app.use(exp.json())
+
+app.use('/auth'    , authRtr    );
+app.use('/students', studentsRtr);
+app.use('/teachers', teachersRtr);
 
 app.listen(port, () => {
     console.log(`Listening on 0.0.0.0:${port}...`);
